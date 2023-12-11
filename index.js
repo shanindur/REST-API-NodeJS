76f6290d15dd814cdeebdd26bcbf6a85fc68e292
Shanindu Rajapaksha <shanindurajapaksha@shanindus-air.home>
2023-12-10 22:11:22 -0500

    implemented GET and POST requests

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const PORT = 8080;
+
+app.use( express.json() );
+
+//GET request
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        name: "Tommy",
+        Size: "medium"
+    })
+}
+);
+
+//POST request
+app.post('/tshirt/:id', (req, res) => {
+
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo) {
+        res.status(418).send({ message: 'We need a logo!' })
+    }
+
+    res.status(200).send(
+        {
+            tshirt: `🙂 with you ${logo} and ID of ${id}`,
+        }
+    )
+}
+);
 
 app.listen(
     PORT,
